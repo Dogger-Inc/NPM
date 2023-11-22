c53d40feaf56b3a41e0277f9d3025cbb819ad17f
Lucas Boinet <lucas@skyloud.fr>
2023-11-22 16:31:07 +0100

    fix: include axios adapter dependencies

--- src/init.ts
+++ src/init.ts
@@ -3,8 +3,8 @@ import Dogger, { InitConfig } from './Dogger';
 export default function (config: InitConfig) {
 	const isClient = typeof window !== 'undefined' && typeof document !== 'undefined';
 	new Dogger({
-		...config, 
 		url: 'http://127.0.0.1:8000',
+		...config, 
 		isClient 
 	});
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+	optimizeDeps: {
+		include: ['axios'],
+	},
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
