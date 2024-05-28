abab52806ae40366f67890d7e9bbdb8f4d0eb26a
Lucas B <lucas@skyloud.fr>
2024-05-28 17:26:42 +0200

    fix: change default url to be dogger server

--- src/init.ts
+++ src/init.ts
@@ -3,7 +3,7 @@ import Dogger, { InitConfig } from './Dogger';
 export default function (config: InitConfig) {
 	const isClient = typeof window !== 'undefined' && typeof document !== 'undefined';
 	return new Dogger({
-		url: 'http://127.0.0.1:8000',
+		url: 'https://dogger.cloud',
 		...config, 
 		isClient 
 	});
